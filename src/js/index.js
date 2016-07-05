268744e080e7668e590141a666221853c5fc100a
Christine Koppelt <cko@users.noreply.github.com>
2016-07-05 22:55:35 +0200

    add location

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,13 @@
 function initialize() {
     var mapCanvas = document.getElementById('map');
     var mapOptions = {
-        center: new google.maps.LatLng(48.19, 11.6472),
+        center: new google.maps.LatLng(48.1, 11.546),
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
         }
     var map = new google.maps.Map(mapCanvas, mapOptions);
     var marker = new google.maps.Marker({
-        position: {lat: 48.1861991, lng: 11.6547822},
+        position: {lat: 48.09702, lng: 11.5452},
         map: map
         });
 }
