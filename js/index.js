79facafd61d713210dfb1ec5879998841cfa8825
Travis-CI <w+travis-for-jscraftcamp@kriesing.de>
2016-07-05 22:46:57 +0000

    Travis deployed 'master' - Tue Jul  5 22:46:57 UTC 2016

--- js/index.js
+++ js/index.js
@@ -5,12 +5,34 @@ function initialize() {
         center: new google.maps.LatLng(48.1, 11.546),
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
+    };
     var map = new google.maps.Map(mapCanvas, mapOptions);
+
+
+    var styles = [
+        {
+            stylers: [
+                { hue: "#2196f3" },
+                { saturation: -50 }
+            ]
+        },{
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [
+                { lightness: 100 },
+                { visibility: "simplified" }
+            ]
+        }
+    ];
+    var styledMap = new google.maps.StyledMapType(styles,
+        {name: "Styled Map"});
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
+
     var marker = new google.maps.Marker({
         position: {lat: 48.09702, lng: 11.5452},
         map: map
-        });
+    });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
