737ddc84afcfd87b94583aaf5c0f8e9d459311e4
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-09-17 19:06:54 +0200

    Remove scaffolding tests, not worth keeping them around.

--- test/tag-stats.js
+++ test/tag-stats.js
@@ -5,19 +5,6 @@ const hasProperties = hamjest.hasProperties;
 const tagStatsForParticipants = require('../src/js/tag-stats');
 
 describe('Tag stats (get collected for each participant)', () => {
-  it('only unique tags for one user, report the same as stats', () => {
-    const tags = ['one', 'two'];
-    const stats = tagStatsForParticipants([tags]);
-    assertThat(stats.length, equalTo(2));
-    assertThat(stats[0], hasProperties({ name: 'one', count: 1}));
-    assertThat(stats[1], hasProperties({name: 'two', count: 1}));
-  });
-  it('unique tags for two users, report the same as stats', () => {
-    const stats = tagStatsForParticipants([['one', 'two'], ['three']]);
-    assertThat(stats[0], hasProperties({name: 'one', count: 1}));
-    assertThat(stats[1], hasProperties({name: 'two', count: 1}));
-    assertThat(stats[2], hasProperties({name: 'three', count: 1}));
-  });
   it('overlapping tags for many users', () => {
     const stats = tagStatsForParticipants([['one', 'two'], ['three'], ['one', 'three']]);
     assertThat(stats[0], hasProperties({name: 'one', count: 2}));
