f51cda65cee792c622a7b0f4043253a479ebdfc3
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-09-17 18:49:50 +0200

    Refactor.

--- src/js/tag-stats.js
+++ src/js/tag-stats.js
@@ -8,30 +8,34 @@ function tagStatsForParticipants(tagLists) {
     }
     return handlebarsCompatible;
   }
-  return toHandlebarsCompatibleList(innerTagStatsForParticipants(tagLists));
+  return toHandlebarsCompatibleList(tagStatsForParticipantsAsObject(tagLists));
 }
 
-function innerTagStatsForParticipants(tagLists) {
+function cloneObj(obj) {
+  var clone = {};
+  for (var key in obj) {
+    clone[key] = obj[key];
+  }
+  return clone;
+}
+
+function addCounts(cloned, addOnto) {
+  for (var key in addOnto) {
+    var keyExistsAlready = key in cloned;
+    var valueInCloned = keyExistsAlready ? cloned[key] : 0;
+    cloned[key] = valueInCloned + addOnto[key];
+  }
+  return cloned;
+}
+
+function tagStatsForParticipantsAsObject(tagLists) {
   function sumUp(tags1, tags2) {
-    var sum = {};
-    // copy all tags1 content
-    for (var key in tags1) {
-      sum[key] = tags1[key];
-    }
-    // add tags2 onto it
-    for (var key in tags2) {
-      if (key in sum) {
-        sum[key] += tags2[key];
-      } else {
-        sum[key] = tags2[key];
-      }
-    }
-    return sum;
+    return addCounts(cloneObj(tags1), tags2);
   }
   if (tagLists.length === 1) {
     return statsForOneParticipant(tagLists[0]);
   }
-  return sumUp(statsForOneParticipant(tagLists[0]), innerTagStatsForParticipants(tagLists.slice(1)));
+  return sumUp(statsForOneParticipant(tagLists[0]), tagStatsForParticipantsAsObject(tagLists.slice(1)));
 }
 
 function statsForOneParticipant(tags) {
