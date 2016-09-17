f1ae406910f1a9c25d16d6900581f151a393a09e
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-09-17 14:19:10 +0200

    A bit refactoring.

--- src/js/tshirt-sizes.js
+++ src/js/tshirt-sizes.js
@@ -1,20 +1,37 @@
-function sortShirtSizes(shirts) {
+(function (){
+
+  function updateForSize(curShape, size) {
+    if (!curShape[size]) {
+      curShape[size] = 1;
+    } else {
+      curShape[size]++;
+    }
+  }
+
+  function updateForShape(sizesObj, shape) {
+    if (!sizesObj[shape]) {
+      sizesObj[shape] = { total: 0 };
+    }
+    sizesObj[shape].total++;
+  }
+
+  function sortShirtSizes(shirts) {
   function buildSizesObject(sizesObj, cur) {
     var split = cur.split('-');
     var shape = split[0]; // M or W
     var size = split[1]; // L, XL, ...
-    if (!sizesObj[shape]) sizesObj[shape] = { total: 0 };
-    sizesObj[shape].total++;
-    if (!sizesObj[shape][size]) {
-      sizesObj[shape][size] = 1;
-    } else {
-      sizesObj[shape][size]++;
-    }
+    updateForShape(sizesObj, shape);
+    updateForSize(sizesObj[shape], size);
     sizesObj.total++;
     return sizesObj;
   }
   return shirts.reduce(buildSizesObject, { total: 0 });
 }
+
 if ('undefined'!=typeof module && module.exports) {
   module.exports = sortShirtSizes;
+} else {
+  // when loaded in the browser
+  window.sortShirtSizes = sortShirtSizes;
 }
+})();
