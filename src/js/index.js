e72f00617899ab9c7e0a005fd1cc575d4cb0cf58
pyous <ylharri@hotmail.fr>
2016-10-02 02:16:44 +0100

    Update Index.js (Adding Smooth Scrolling)
    Performs a smooth page scroll to an anchor on the same page.

--- src/js/index.js
+++ src/js/index.js
@@ -42,3 +42,19 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+//Adding the smooth scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
