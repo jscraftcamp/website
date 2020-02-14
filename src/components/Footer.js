813242ebfddc1e22c4432c36376a39e34cc7ac32
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2020-02-14 19:28:39 +0100

    add netlify link to footer as needed for netlify sponsoring

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,44 +7,60 @@
 
 import React from "react"
 
-import _data from '../_data.json'
+import { baseColor, navigationItems } from "../_data.json"
 
 const Footer = () => {
   return (
     <footer>
       <div>
-        <h3 className={_data.baseColor + "-text"}>Sitemap</h3>
+        <h3 className={baseColor + "-text"}>Sitemap</h3>
         <ul>
-          { _data.navigationItems.main.map(item => <li key={item.title}>
-            <a href={item.url} className={_data.baseColor + "-text"}>
+          {navigationItems.main.map(item => <li key={item.title}>
+            <a href={item.url} className={baseColor + "-text"}>
               {item.title}
             </a>
-             </li>)
+          </li>)
           }
         </ul>
       </div>
       <div>
-        <h3 className={_data.baseColor + "-text"}>Get in touch</h3>
+        <h3 className={baseColor + "-text"}>Get in touch</h3>
         <ul>
-          <li><a className={_data.baseColor + "-text"} href="/theteam">The Team</a></li>
-          <li><a className={_data.baseColor + "-text"} href="https://twitter.com/jscraftcamp">Twitter</a></li>
-          <li><a className={_data.baseColor + "-text"} href="/imprint.html">Imprint</a></li>
-          <li><a className={_data.baseColor + "-text"} href="/privacypolicy">Privacy Policy</a></li>
+          <li><a className={baseColor + "-text"} href="/theteam">The Team</a></li>
+          <li><a className={baseColor + "-text"} href="https://twitter.com/jscraftcamp">Twitter</a></li>
+          <li><a className={baseColor + "-text"} href="/imprint.html">Imprint</a></li>
+          <li><a className={baseColor + "-text"} href="/privacypolicy">Privacy Policy</a></li>
         </ul>
       </div>
-      <div style={{"flex": 1.5}}>
-        <h3 className={_data.baseColor + "-text"}>Archive</h3>
+      <div style={{ "flex": 1.5 }}>
+        <h3 className={baseColor + "-text"}>Archive</h3>
         <ul>
-          {_data.navigationItems.archive.map(item => <li key={item.title}><a
-            className={_data.baseColor + "-text"} href={item.url + '.html'}>{item.title}</a></li>)
+          {navigationItems.archive.map(item => <li key={item.title}><a
+            className={baseColor + "-text"} href={item.url + ".html"}>{item.title}</a></li>)
           }
+        </ul>
+      </div>
+      <div style={{ "flex": 1.5 }}>
+        <h3 className={baseColor + "-text"}>Built with</h3>
+        <ul>
+          <li><a href="https://www.netlify.com">
+            <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg"/>
+          </a></li>
+          <li><a href="https://gatsbyjs.org">
+              <img alt="Gatsby" src="https://www.gatsbyjs.org/monogram.svg"
+                   style={{
+                     verticalAlign: "middle",
+                     padding: 4,
+                     width: 38
+                   }}
+              />
+              <span>Gatsby v2</span>
+          </a></li>
         </ul>
       </div>
     </footer>);
 }
 
-Footer.propTypes = {
-};
-
 export default Footer
+
 
