51102dc1611522fc0097c3e705de1194aeee21b5
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2020-03-11 20:46:01 +0100

    formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home"/>
     <>
-      <div className="container logo centered">
-        <div style={
-          {display: 'flex', alignItems: 'left'}
-        }>
+      <div className="container logo">
+        <div style={{ display: "flex" }}>
           <Image src="/src/images/new_logo.png" alt="logo" height={300} />
         </div>
       </div>
@@ -22,10 +20,16 @@ const IndexPage = () => (
       <div className="container">
         <div className="row">
           <div className="col s12 m12">
-            <h1 className={`header center ${_data.baseColor}-text`}>JSCraftCamp 2020</h1>
+            <h1 className={`header center ${_data.baseColor}-text`}>
+              JSCraftCamp 2020
+            </h1>
             <h3 className="header center">Will return to Munich, July 10th - 11th 2020!</h3>
-            <h3 className="header center light">A BarCamp about JavaScript and Crafting Software</h3>
-            <h3 className="header center light"><a href="/photos2018.html">Impressions from 2018</a></h3>
+            <h3 className="header center light">
+              A BarCamp about JavaScript and Crafting Software
+            </h3>
+            <h3 className="header center light">
+              <a href="/photos2018.html">Impressions from 2018</a>
+            </h3>
           </div>
         </div>
       </div>
@@ -34,10 +38,19 @@ const IndexPage = () => (
         <div className="row">
           <div className="col s12 m4 offset-m4">
             <h4 className={`header center ${_data.baseColor}-text`}>
-              Follow us on <a href="https://twitter.com/JSCraftCamp">Twitter&nbsp;<img src="img/twitter.png" alt="twitter logo"
-                                                                                       style={{width: 25}}/></a>
+              Follow us on{" "}
+              <a href="https://twitter.com/JSCraftCamp">
+                Twitter&nbsp;
+                <img
+                  src="img/twitter.png"
+                  alt="twitter logo"
+                  style={{ width: 25 }}
+                />
+              </a>
             </h4>
-            <p style={{textAlign: 'center'}}>Be the first to know informations about JSCC20!</p>
+            <p style={{ textAlign: "center" }}>
+              Be the first to know informations about JSCC20!
+            </p>
           </div>
         </div>
       </div>
@@ -46,17 +59,24 @@ const IndexPage = () => (
         <div className="row flow-text">
           <div className="col s12 m6">
             <h5 className="header center">JavaScript</h5>
-            <p className="block">The focus of this BarCamp are JavaScript and related technologies. Possible topics are:
-              Popular frameworks like Angular and React, the latest ECMAScript standard, languages like Elm or
-              TypeScript which transpile to JavaScript, server-side programming with node.js, paradigms like event
-              driven programming or future technologies like WebAssembly.</p>
+            <p className="block">
+              The focus of this BarCamp are JavaScript and related technologies.
+              Possible topics are: Popular frameworks like Angular and React,
+              the latest ECMAScript standard, languages like Elm or TypeScript
+              which transpile to JavaScript, server-side programming with
+              node.js, paradigms like event driven programming or future
+              technologies like WebAssembly.
+            </p>
           </div>
           <div className="col s12 m6">
             <h5 className="header center">Crafting Software</h5>
-            <p className="block">Have you ever struggled with an huge old JavaScript codebase? Or rolled your eyes about
-              an <code>'undefined' is not a function</code> error? We will discuss which tools, practices and
-              architecture patterns – from continuous integration to modular components – can help to create and
-              maintain long-living, evolvable JavaScript applications.</p>
+            <p className="block">
+              Have you ever struggled with an huge old JavaScript codebase? Or
+              rolled your eyes about an <code>'undefined' is not a function</code> error? We will discuss
+              which tools, practices and architecture patterns – from continuous
+              integration to modular components – can help to create and
+              maintain long-living, evolvable JavaScript applications.
+            </p>
           </div>
         </div>
         <div className="row flow-text">
@@ -72,11 +92,18 @@ const IndexPage = () => (
           </div>
           <div className="col s12 m6">
             <h5 className="header center">How to register?</h5>
-            <p className="block">Attendance is <strong>free of charge</strong> and we encourage to sign up in advance.
-              Registration happens by submitting your name and some other information
-              via <a href="http://stackoverflow.com/questions/14680711/how-to-do-a-github-pull-request" target="_blank">Pull
-                Request</a>.
-              More infos and explanations can be found on the <Link to="registration">Registration Page.</Link>
+            <p className="block">
+              Attendance is <strong>free of charge</strong> and we encourage to
+              sign up in advance. Registration happens by submitting your name
+              and some other information
+              via <a
+                href="http://stackoverflow.com/questions/14680711/how-to-do-a-github-pull-request"
+                target="_blank"
+              >
+                Pull Request
+              </a>
+              . More infos and explanations can be found on the{" "}
+              <Link to="registration">Registration Page.</Link>
             </p>
           </div>
         </div>
