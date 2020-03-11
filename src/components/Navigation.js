e495318413f654a7ec0698f54cd00b66dab9238c
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2020-03-11 20:19:38 +0100

    workaround for broken navigation on mobile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,26 +1,17 @@
 import React from "react"
 
-import _data from '../_data';
+import { baseColor, navigationItems } from "../_data"
 
-export default () => <nav className={_data.baseColor}>
+export default () =>
+  <nav className={baseColor}>
     <div className="nav-wrapper container">
-        <a href="/" className="brand-logo">JS CraftCamp</a>
-        <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
-
-        <ul id="nav-desktop" className="right hide-on-med-and-down">
-            {_data.navigationItems.main.map(item => (
-                <li key={item.title}><a href={item.url} className={item.cssClasses}>{item.title}</a></li>
-            ))}
-            <li><a href="https://twitter.com/jscraftcamp"><i className="fa fa-twitter fa-fw"/></a></li>
-            <li><a href="https://github.com/jscraftcamp/website"><i className="fa fa-github fa-fw"/></a></li>
-        </ul>
-
-        <ul id="mobile-demo" className="side-nav">
-            {_data.navigationItems.main.map(item => (
-                <li key={item.title}><a href={item.url} className={item.cssClasses}>{item.title}</a></li>
-            ))}
-            <li><a href="https://twitter.com/jscraftcamp"><i className="fa fa-twitter fa-fw"/></a></li>
-            <li><a href="https://github.com/jscraftcamp/website"><i className="fa fa-github fa-fw"/></a></li>
-        </ul>
+      <a href="/" className="brand-logo left hide-on-small-and-down">JS CraftCamp</a>
+      <ul id="nav-desktop" className="right hide-on-med-and-downX">
+        {navigationItems.main.map(item => (
+          <li key={item.title}><a href={item.url} className={item.cssClasses}>{item.title}</a></li>
+        ))}
+        <li><a href="https://twitter.com/jscraftcamp"><i className="fa fa-twitter fa-fw"/></a></li>
+        <li><a href="https://github.com/jscraftcamp/website"><i className="fa fa-github fa-fw"/></a></li>
+      </ul>
     </div>
-</nav>
+  </nav>
