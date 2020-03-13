1e7cfd89324a9571d952c8c3b1cb3ddaff059463
Michael Zoidl <github@michaelzoidl.com>
2020-03-13 17:13:58 +0100

    add values page

--- src/pages/values.js
+++ src/pages/values.js
@@ -1,41 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
+import Page from "../components/page"
+import CodeOfConduct from "../components/code-of-conduct"
 
-import Layout from "../DEPRECATED_components/layout"
-import SEO from "../DEPRECATED_components/seo"
-
-const Values = () => (
-  <Layout>
-    <SEO title="values" description="code of conduct" />
-    <div className="container">
-      <div className="row flow-text">
-        <div className="col s12 l10 offset-l1">
-          <h5 className="header center">Our Code of Conduct</h5>
-          <p className="block">
-            We are committed to providing a friendly, safe and welcoming
-            environment for all, regardless of gender, sexual orientation,
-            programming language, ability, ethnicity, socioeconomic status, and
-            religion (or lack thereof).
-          </p>
-          <p className="block">
-            We believe in people and our guests - so we ask nothing more of
-            everybody than to be <strong>respectful</strong> and{" "}
-            <strong>friendly</strong> with one another.
-          </p>
-          <p className="block">
-            And so we invite all those who participate in the JSCraftCamp and
-            the community surrounding the conference to help us create safe and
-            positive experiences for everyone.
-          </p>
-          <p>
-            If you have any questions or problems regarding this Code of
-            Conduct, don't hesitate to contact one of us from{" "}
-            <Link to={"theteam"}>the team.</Link>
-          </p>
-        </div>
-      </div>
-    </div>
-  </Layout>
+export default () => (
+  <Page title="Location">
+    <CodeOfConduct />
+  </Page>
 )
-
-export default Values
