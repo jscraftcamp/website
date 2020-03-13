c961fdf9c635386717c0d444377678fac29964b3
Michael Zoidl <github@michaelzoidl.com>
2020-03-13 17:13:47 +0100

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,12 @@
 import React from "react"
 import Page from "../components/page"
+import Section from "../components/section"
+import Intro from "../components/intro"
 
-export default () => <Page title="Home">Home Site</Page>
+export default () => (
+  <Page title="Location">
+    <Section>
+      <Intro />
+    </Section>
+  </Page>
+)
