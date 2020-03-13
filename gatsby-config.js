5c9d2069fed21dcf57d7c6a76c0ad96faa310333
Michael Zoidl <github@michaelzoidl.com>
2020-03-13 17:10:43 +0100

    update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,42 @@
 module.exports = {
   siteMetadata: {
-    title: `JSCraftCamp - 2020 in Munich`,
-    description: `Website for JSCraftCamp - 2020 in Munich`,
-    author: `@davelosert, @rhosts, @michaelzoidl, @Narigo`,
+    title: "JSCraftCamp - 2020 in Munich",
+    description: "Website for JSCraftCamp - 2020 in Munich",
+    author: "@davelosert, @rhosts, @michaelzoidl, @Narigo",
   },
   pathPrefix: "/",
   plugins: [
-    `gatsby-transformer-json`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets`,
-      },
-    },
+    "gatsby-transformer-json",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-svg-sprite",
     {
       resolve: "gatsby-plugin-sass",
       options: {
         data: `
+          @import "node_modules/sass-mq/_mq.scss";
           @import "${__dirname}/src/config.scss";
         `,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `JSCraftCamp - 2020 in Munich`,
-        short_name: `JSCraftCamp`,
-        start_url: `/`,
-        background_color: `#ececec`,
-        theme_color: `#ececec`,
-        display: `minimal-ui`,
-        icon: `src/assets/logo.png`,
+        name: "images",
+        path: `${__dirname}/src/assets`,
+      },
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "JSCraftCamp - 2020 in Munich",
+        short_name: "JSCraftCamp",
+        start_url: "/",
+        background_color: "#ececec",
+        theme_color: "#ececec",
+        display: "minimal-ui",
+        icon: "src/assets/logo.png",
       },
     },
   ],
