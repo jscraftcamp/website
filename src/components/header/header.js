5cf4be99141791946456f51535939c0b2f955597
Michael Zoidl <github@michaelzoidl.com>
2020-03-13 17:12:43 +0100

    update header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,38 @@
 import React from "react"
 import style from "./header.module.scss"
 
-export default () => <div className={style.root}>Header</div>
+import Typography from "../typography"
+import Button from "../button"
+import Section from "../section"
+
+import { Link, navigate } from "gatsby"
+
+export default () => (
+  <Section className={style.root}>
+    <div className={style.logo}>
+      <Typography
+        component={Link}
+        variants={["logo", "link", "noselect"]}
+        to="/"
+      >
+        JSCraftCamp
+      </Typography>
+    </div>
+    <div className={style.navigation}>
+      <Typography component={Link} variant="link" to="/">
+        Home
+      </Typography>
+      <Typography
+        component="a"
+        variant="link"
+        href="https://github.com/jscraftcamp/website/projects/10"
+      >
+        Sponsoring
+      </Typography>
+      <Typography component={Link} variant="link" to="/values">
+        Values
+      </Typography>
+      <Button onClick={() => navigate("/register")}>Register</Button>
+    </div>
+  </Section>
+)
