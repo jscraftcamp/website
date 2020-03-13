d6fa08b628332ff0cba613dcd7024162a5f231f7
Michael Zoidl <github@michaelzoidl.com>
2020-03-13 17:13:58 +0100

    add values page

---
+++ src/pages/values.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Page from "../components/page"
+import CodeOfConduct from "../components/code-of-conduct"
+
+export default () => (
+  <Page title="Location">
+    <CodeOfConduct />
+  </Page>
+)
