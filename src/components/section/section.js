816b2a8dc427b4afe9955ddeb5602ae3e0dadbe8
Michael Zoidl <github@michaelzoidl.com>
2020-03-13 17:11:49 +0100

    create section component
    (cherry picked from commit 4dbd9bc7aa3925433d87df09cebbe43fbcd0163e)

---
+++ src/components/section/section.js
@@ -0,0 +1,10 @@
+import React from "react"
+import style from "./section.module.scss"
+
+import cn from "classnames"
+
+export default ({ className, children }) => (
+  <div className={cn(style.root)}>
+    <div className={cn(style.content, className)}>{children}</div>
+  </div>
+)
---
+++ src/components/section/section.story.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Section from "./"
+
+export default {
+  title: "components|Section",
+  component: Section,
+}
+
+export const base = () => <Section>Hello World!</Section>
