fa2bbb24fad05cc127046918d758caee0ee4744d
Michael Zoidl <github@michaelzoidl.com>
2020-03-15 16:03:27 +0100

    add explanation section in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,15 @@ import Page from "../components/page"
 import Section from "../components/section"
 import Intro from "../components/intro"
 import Spacer from "../components/spacer"
+import Explanation from "../components/explanation"
 
 export default () => (
   <Page title="Location">
     <Section>
       <Spacer size="xl" />
       <Intro />
+      <Spacer size="xl" />
+      <Explanation />
     </Section>
   </Page>
 )
