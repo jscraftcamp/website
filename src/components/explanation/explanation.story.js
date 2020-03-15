627d7f3215c1070c936f4c73261449468332e5b5
Michael Zoidl <github@michaelzoidl.com>
2020-03-15 16:03:17 +0100

    create explanation section

---
+++ src/components/explanation/explanation.story.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Explanation from "./"
+
+export default {
+  title: "sections|Explanation",
+  component: Explanation,
+}
+
+export const base = () => <Explanation />
