ecb3dbb16279473cc803518745a4e223ad4c266b
Michael Zoidl <github@michaelzoidl.com>
2020-03-15 13:54:34 +0100

    finish intro component

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -3,13 +3,17 @@ import style from "./intro.module.scss"
 
 import Icon from "../icon"
 import Typography from "../typography"
+import Spacer from "../spacer"
 
-import Logo from "../../assets/images/logo.svg"
+import logo from "../../assets/images/logo.svg"
+import twitter from "../../assets/images/twitter.svg"
+import github from "../../assets/images/github.svg"
+import skyline from "../../assets/images/skyline.svg"
 
 export default () => (
   <div className={style.root}>
     <div className={style.logo}>
-      <Icon source={Logo} />
+      <Icon source={logo} />
     </div>
     <div className={style.text}>
       <Typography className={style.headline} component="h1">
@@ -19,6 +23,16 @@ export default () => (
         Will return to Munich, this summer!
         <br />A Barcamp about JavaScript and Crafting Software
       </Typography>
+      <Spacer size="xs" />
+      <div className={style.social}>
+        <a href="https://twitter.com/jscraftcamp">
+          <Icon className={style.socialIcon} source={twitter} />
+        </a>
+        <a href="https://github.com/jscraftcamp/website">
+          <Icon className={style.socialIcon} source={github} />
+        </a>
+      </div>
+      <Icon className={style.skyline} source={skyline} />
     </div>
   </div>
 )
