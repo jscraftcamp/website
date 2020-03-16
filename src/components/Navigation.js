f34b1a3b6e8e3c6c30e7a48305af3f8279d37be7
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2020-03-17 00:07:44 +0100

    Navigation Header still has dependency on fontawesome icon (github, twitter)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import { baseColor, navigationItems } from "../_data"
+import '../fonts/font-awesome/css/font-awesome.css'
 
 export default () =>
   <nav className={baseColor}>
