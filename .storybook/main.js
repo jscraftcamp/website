e64e7fd06098fcd7c57452cbdfa9efe953bdade0
Michael J. Zoidl <github@michaelzoidl.com>
2020-03-23 13:10:31 +0100

    Setup Storybook (#870)
    * setup storybook with custom configurations for gatsby
    
    * prepare footer for storybook
    
    * move storybook/viewport addon a dev-dependency

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+module.exports = {
+  stories: ["../src/components/**/*.story.js"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-viewport/register",
+  ],
+  webpackFinal: config => {
+    // Prepare storybook for Gatsby
+    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+    config.module.rules[0].use[0].loader = require.resolve("babel-loader")
+    config.module.rules[0].use[0].options.presets = [
+      require.resolve("@babel/preset-react"),
+      require.resolve("@babel/preset-env"),
+    ]
+    config.module.rules[0].use[0].options.plugins = [
+      require.resolve("@babel/plugin-proposal-class-properties"),
+      require.resolve("babel-plugin-remove-graphql-queries"),
+    ]
+    config.resolve.mainFields = ["browser", "module", "main"]
+
+    return config
+  },
+}
