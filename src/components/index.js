22086678d4b9426a4209baa39c3e9bf746236d47
Michael Zoidl <github@michaelzoidl.com>
2020-03-23 16:01:06 +0100

    create typography component

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as Footer } from "./footer/footer"
+export { default as Typography } from "./typography/typography"
---
+++ src/components/typography/typography.story.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Typography } from "../"
+
+export default {
+  title: "components|Typography",
+  component: Typography,
+}
+
+export const base = () => <Typography>Hello World</Typography>
+export const h1 = () => <Typography component="h1">Hello World</Typography>
