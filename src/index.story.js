89c4e15f6ba275c3348057699ef2163be6be82da
lowsky <lowsky@users.noreply.github.com>
2020-06-19 00:45:47 +0200

    relocate deprecated pages
    (cherry picked from commit c4a6b227344ef53f0edd5cd21c5ab0d6f6248e4c)

--- src/index.story.js
+++ src/index.story.js
@@ -1,17 +1,11 @@
 import React from "react"
 
 import Index from "./pages"
-import Imprint from "./pages/imprint"
-import TheTeam from "./pages/theteam"
-import NotFound from "./pages/404"
-import PrivacyPolicy from "./pages/privacypolicy"
+import Register from "./pages/register.js"
 
 export default {
   title: "Pages|main",
 }
 
 export const IndexPage = () => <Index />
-export const ImprintPage = () => <Imprint />
-export const TheTeamPage = () => <TheTeam />
-export const NotFoundPage = () => <NotFound />
-export const PrivacyPolicyPage = () => <PrivacyPolicy />
+export const RegisterPage = () => <Register />
