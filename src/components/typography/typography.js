fa63ed8c211553739fe61f8db079b202d478e8f3
lowsky <lowsky@users.noreply.github.com>
2020-06-19 02:25:32 +0200

    let typography handle different variants

--- src/components/typography/typography.js
+++ src/components/typography/typography.js
@@ -2,16 +2,25 @@ import React from "react"
 import style from "./typography.module.scss"
 import cn from "classnames"
 
-export default ({ className, component, children, ...props }) => {
-  // if no component is given we use <span> as default tag
-  const Component = component || "span"
+export default ({
+                  className,
+                  component,
+                  variant,
+                  variants = [],
+                  paragraph,
+                  children,
+                  ...props
+                }) => {
+  const Component = component || (paragraph ? "p" : "span")
 
   return (
     <Component
       className={cn(
         className,
         style.root,
-        style[Component] // Component returns the "string" of the tag not a node
+        style[Component], // Component returns the "string" of the tag not a node
+        style[variant],
+        ...variants.map(v => style[v])
       )}
       {...props}
     >
