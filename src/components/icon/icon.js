272cd8f9992e9b3a385afad88d89d25b1df981e9
lowsky <lowsky@users.noreply.github.com>
2021-04-09 13:36:55 +0200

    fix gatsby config for local storybook
    found
    https://github.com/gatsbyjs/gatsby/issues/26099#issuecomment-691288286
    
    https://github.com/IsaiahByDayah/personal-site/commit/202dc7681148200db71cf465838d87410860d4fd

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -1,19 +1,23 @@
 import React from "react"
 
 const Icon = ({ className, source : Source }) => {
-debugger
   return (
   <svg
     className={className}
     version="1.1"
-    viewBox={Source.defaultProps?.viewBox}
+    viewBox={Source.defaultProps?.viewBox ?? Source.viewBox}
     width={Source.defaultProps?.width}
     height={Source.defaultProps?.height}
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
   >
-    <Source />
-   </svg>
+      {
+        // Workaround for storybook
+        // in storybook, we only get a BrowserSpriteSVG object instead of
+        // a react component
+        Source.id ? <use xlinkHref={`#${Source.id}`} /> : <Source />
+      }
+  </svg>
 )
 }
 export default Icon
