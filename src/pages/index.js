d3acbb372152dc96a9f529c7eee0da75388c3c5d
lowsky <lowsky@users.noreply.github.com>
2022-05-25 09:26:18 +0200

    add a sponsors section

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Section from "../components/section"
 import Intro from "../sections/intro"
 import Spacer from "../components/spacer"
 import Explanation from "../sections/explanation"
+import Sponsors from "../sections/sponsors"
 
 const IndexPage = () => (
   <Page title="JSCraftCamp">
@@ -12,6 +13,8 @@ const IndexPage = () => (
       <Intro />
       <Spacer size="xl" />
       <Explanation />
+      <Spacer size="xl" />
+      <Sponsors />
     </Section>
   </Page>
 )
