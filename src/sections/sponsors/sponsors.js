16ccd7da768f003e3b2d4327ee8add1784772dc1
lowsky <217931+lowsky@users.noreply.github.com>
2022-05-30 08:00:47 +0200

    Improve sponsoring section (#1156)
    * remove border-shadow, have white background images, update peerigon and grollmus images
    
    * fix sponsors story
    
    * add links to sponsor logos
    
    * remove redundant section wrapper, add noreferrer
    
    Co-authored-by: lowsky <lowsky@users.noreply.github.com>

--- src/sections/sponsors/sponsors.js
+++ src/sections/sponsors/sponsors.js
@@ -1,6 +1,5 @@
 import React from "react"
 
-import Section from "../../components/section"
 import TextBlock from "../../components/text-block"
 import Typography from "../../components/typography"
 
@@ -8,26 +7,41 @@ import style from "./sponsors.module.scss"
 
 const Sponsors = () => {
   const sponsors = [
-    { src: "/img/cc_logo.png" },
-    { src: "/img/sponsor-2022/grollmus.png" },
-    { src: "/img/sponsor-2022/compose-us.png" },
-    { src: "/img/sponsor-2019/tng.png" },
-    { src: "/img/sponsor-2019/peerigon.png" },
+    {
+      src: "/img/cc_logo.png",
+      link: "https://www.codecentric.de/",
+    },
+    {
+      src: "/img/sponsor-2022/grollmus.png",
+      link: "https://www.grollmus.de/",
+    },
+    {
+      src: "/img/sponsor-2022/compose-us.png",
+      link: "https://www.compose.us/",
+    },
+    {
+      src: "/img/sponsor-2019/tng.png",
+      link: "https://www.tngtech.com/",
+    },
+    {
+      src: "/img/sponsor-2022/peerigon.png",
+      link: "https://www.peerigon.com/",
+    },
   ]
   return (
     <>
-      <Section className={style.root}>
-        <TextBlock headlineComponent="h1" headline="Sponsors">
-          <Typography paragraph>Our great sponsors:</Typography>
-        </TextBlock>
-      </Section>
+      <TextBlock headlineComponent="h1" headline="Sponsors">
+        <Typography paragraph>Our great sponsors:</Typography>
+      </TextBlock>
       <div className={style.row}>
         {sponsors.map((s, i) => {
           return (
             <div className={style.col} key={i}>
-              <div className={style.image} key={i}>
-                <img alt={"sponsor #" + i} src={s.src} width="100%" />
-              </div>
+              <a href={s.link} rel="noopener noreferrer" target="_blank">
+                <div className={style.image} key={i}>
+                  <img alt={"sponsor #" + i} src={s.src} width="100%" />
+                </div>
+              </a>
             </div>
           )
         })}
