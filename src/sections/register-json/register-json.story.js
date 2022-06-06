498a55ba6e38f21c3b887717fafd35816551fad3
lowsky <lowsky@users.noreply.github.com>
2022-06-06 22:42:14 +0200

    refactor our register component

---
+++ src/sections/register-json/register-json.story.js
@@ -0,0 +1,9 @@
+import React from "react"
+import RegisterJson from "./index"
+
+export default {
+  title: "sections/RegisterJson",
+  component: RegisterJson,
+}
+
+export const base = () => <RegisterJson />
