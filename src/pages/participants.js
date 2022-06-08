e6cdd921e4d0653c6a1f78dbb1388d104662fced
lowsky <lowsky@users.noreply.github.com>
2022-06-08 13:50:08 +0200

    add word cloud to participants page

--- src/pages/participants.js
+++ src/pages/participants.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import WordCloud from "react-d3-cloud"
 
 import Page from "../components/page"
 import Section from "../components/section"
@@ -33,11 +34,33 @@ export const query = graphql`
 export default function Participants({ data }) {
   const { allParticipantsJson } = data
   const participants = allParticipantsJson.nodes
+
+  const allTags = new Map()
+  participants.forEach(({ tags }) => {
+    tags.forEach((tx) => {
+      const t = tx.toLowerCase()
+      allTags.set(t, (allTags.get(t) ?? 0) + 1)
+    })
+  })
+
+  const wordData = [...allTags].map(([text, value]) => ({
+    text,
+    value,
+  }))
   return (
     <Page title="Participants">
       <Section>
         <TextBlock headline="Participants" />
 
+        <WordCloud
+          data={wordData}
+          width={220}
+          height={120}
+          font="Poppins"
+          fontSize={(word) => Math.log2(word.value) * 3 + 3}
+          spiral="rectangular"
+          padding={1}
+        />
         <ParticipantsCounter participants={participants} />
 
         <Spacer size="m" />
