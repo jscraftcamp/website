686f72b0ba074827ef576e949bf2ccc421821742
lowsky <lowsky@users.noreply.github.com>
2023-03-08 09:46:00 +0100

    get rid of material-ui css

--- src/sections/team/team.js
+++ src/sections/team/team.js
@@ -3,34 +3,28 @@ import React from "react"
 import data from "../../data.json"
 
 import * as styles from "./team.module.scss"
-import "./team.scss"
 
 export const Team = () => (
-  <div className="row">
-    {data.theteam.map((member) => (
-      <div className="col s12 m4 l3" key={member.name}>
-        <div>
-          {member.photo && (
+  <div className={styles.theTeam}>
+    {data.theteam.map(({ name, photo, email }) => {
+      return (
+        <div key={name}>
+          {photo && (
             <img
-              className={styles.responsive_img}
-              alt={"member image " + member.name}
-              src={"/img/theteam/" + member.photo}
+              className={styles.img}
+              src={`/img/theteam/${photo}`}
+              alt={"member image " + name}
             />
           )}
-          <div className={styles.content}>
-            <div className={styles.title}>
-              <span>{member.name}</span>
-              <br />
-              {member?.email && (
-                <a className={styles.mail} href={"mailto:" + member.email}>
-                  {member.email}
-                </a>
-              )}
-            </div>
-          </div>
+          <div>{name}</div>
+          {email && (
+            <a className={styles.mail} href={"mailto:" + email} title={email}>
+              {email}
+            </a>
+          )}
         </div>
-      </div>
-    ))}
+      )
+    })}
   </div>
 )
 
