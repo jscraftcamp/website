7ee80492fc2413125d688e816f1f0f4eab62b2ce
lowsky <lowsky@users.noreply.github.com>
2023-03-12 20:19:28 +0100

    enable gatsby-way of importing styles with storybook

--- src/components/participant-card/participant-card-reveal/participant-card-reveal.js
+++ src/components/participant-card/participant-card-reveal/participant-card-reveal.js
@@ -2,7 +2,7 @@ import React from "react"
 import MiniHeadline from "../../mini-headline"
 import Typography from "../../typography"
 import ParticipantCardTitle from "../participant-card-title"
-import convert4StorybookIfNeeded from "../../gatsbyStylesHandler"
+import convert4StorybookIfNeeded from "../../../gatsbyStylesHandler"
 import * as gatsbyStyles from "./participant-card-reveal.module.scss"
 
 const style = convert4StorybookIfNeeded(gatsbyStyles)
