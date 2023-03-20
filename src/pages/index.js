e0e2ed8e7f274964155bf0d6149d79b5d708bec2
Bernd Kaiser <bernd.kaiser@inovex.de>
2023-03-20 19:43:44 +0100

    Add sponsors 2023

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,14 @@ import Section from "../components/section"
 import Intro from "../sections/intro"
 import Spacer from "../components/spacer"
 import Explanation from "../sections/explanation"
-// uncomment when show new sponsors import Sponsors from "../sections/sponsors"
+import Sponsors from "../sections/sponsors"
 
 const IndexPage = () => (
   <Page title="JSCraftCamp">
     <Section>
       <Intro />
-      {/*<Spacer size="xl" />
-      <Sponsors />
-      */}
+      <Spacer size="xl" />
+        <Sponsors />
       <Spacer size="xl" />
       <Explanation />
       <Spacer size="xl" />
