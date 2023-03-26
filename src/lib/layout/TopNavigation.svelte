0f62cad98ae0475cab84226b19c43a484a3bef4f
Joern Bernhardt <joern.bernhardt@compose.us>
2023-03-26 16:52:14 +0200

    feat: Mobile menu
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/layout/TopNavigation.svelte
+++ src/lib/layout/TopNavigation.svelte
@@ -4,8 +4,16 @@
 </script>
 
 <header>
+	<input id="navbar-open" type="checkbox" />
+	<label for="navbar-open"
+		><img src={logo} alt="JSCraftCamp" /> <span>JSCraftCamp</span>
+		<div class="menu-icon">+</div></label
+	>
 	<nav>
-		<li><a href="{base}/"><img src={logo} alt="JSCraftCamp" /> JSCraftCamp</a></li>
+		<li class="home">
+			<a href="{base}/"><img src={logo} alt="JSCraftCamp" /> JSCraftCamp</a>
+		</li>
+		<li class="mobile-home"><a href="{base}/">Home</a></li>
 		<li><a href="{base}/participants/">Participants</a></li>
 		<li><a href="{base}/sponsoring/">Sponsoring</a></li>
 		<li><a href="{base}/values/">Values</a></li>
@@ -19,13 +27,16 @@
 		max-width: var(--max-page-width);
 	}
 
-	header > nav {
+	nav {
 		display: flex;
 		flex-flow: row wrap;
 		align-items: center;
 		gap: 2em;
-        margin: 0 auto;
+		margin: 0 auto;
 		max-width: var(--max-page-width);
+	}
+	input {
+		display: none;
 	}
 	img {
 		max-height: 1em;
@@ -33,16 +44,74 @@
 	li {
 		list-style: none;
 	}
-	li:first-of-type {
+	li.home,
+	label {
 		flex: 1;
 		font-size: 26px;
 		font-weight: 600;
 		font-family: 'Poppins';
 	}
-	li:first-of-type a {
+	li.home a,
+	label {
 		display: flex;
 		flex-flow: row nowrap;
 		align-items: center;
-		gap: .3em;
+		gap: 0.3em;
+	}
+	li.mobile-home {
+		display: none;
+	}
+	label {
+		display: none;
+		cursor: pointer;
+	}
+	label span {
+		flex: 1;
+	}
+
+	.menu-icon {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 1em;
+		width: 1em;
+		transition: rotate 500ms;
+	}
+
+	@media screen and (max-width: 36em) {
+		label {
+			display: flex;
+		}
+		input[type='checkbox'] + label + nav {
+			display: none;
+		}
+		input[type='checkbox']:checked + label + nav {
+			display: flex;
+		}
+		input[type='checkbox']:checked + label .menu-icon {
+			rotate: calc(3 * 45deg);
+		}
+		nav {
+			align-items: stretch;
+			flex-flow: column;
+			font-size: 1em;
+			gap: 0;
+			margin: 1em 0;
+		}
+		nav li {
+			background: #fff;
+			flex: 1;
+		}
+		nav li a {
+			display: block;
+			padding: 1em;
+			text-align: center;
+		}
+		li.home {
+			display: none;
+		}
+		li.mobile-home {
+			display: block;
+		}
 	}
 </style>
