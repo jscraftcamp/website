131f166fc0dde89549f7b46a48b444bf413e4fd5
Joern Bernhardt <Narigo@users.noreply.github.com>
2023-04-04 22:45:00 +0200

    Use registration date for countdown on main page, add LinkedIn link (#1373)
    * feat: Add LinkedIn and fix countdown
    
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>
    
    * fix: Improve text
    
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>
    
    * feat: Add link to registration page once open
    
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>
    
    ---------
    
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,16 +5,27 @@
 	import Logo from '$lib/layout/Logo.svelte';
 	import discord from './discord.svg';
 	import github from './github.svg';
+	import linkedIn from './linked-in.png';
 	import skyline from './skyline.svg';
 	import twitter from './twitter.svg';
 	import Sponsors from './sponsoring/Sponsors.svelte';
 	import WhatToExpect from './WhatYouCanExpect.svelte';
-	import { eventStartsAt, timeLeft } from '$lib/participants/registration';
+	import {
+		isRegistrationOpen,
+		registrationOpensAt,
+		timeLeft
+	} from '$lib/participants/registration';
+	import { base } from '$app/paths';
 
 	const countdown = writable<string>('');
+	const canRegister = writable<boolean>(isRegistrationOpen());
 	const updateCountdown = () => {
 		const now = +new Date();
-		const { days, hours, minutes, seconds } = timeLeft(now, eventStartsAt);
+		if (registrationOpensAt <= now) {
+			$canRegister = true;
+			return;
+		}
+		const { days, hours, minutes, seconds } = timeLeft(now, registrationOpensAt);
 		const timeAsStringArray = [
 			days > 0 ? `${days} days` : '',
 			hours > 0 ? `${hours} hours` : '',
@@ -24,11 +35,11 @@
 
 		$countdown =
 			timeAsStringArray.length > 1
-				? `That's in ${timeAsStringArray.slice(0, -1).join(', ')} and ${
+				? `That's in <strong>${timeAsStringArray.slice(0, -1).join(', ')} and ${
 						timeAsStringArray[timeAsStringArray.length - 1]
-				  }.`
+				  }</strong>.`
 				: timeAsStringArray.length === 1
-				? `Only ${timeAsStringArray[0]} left!`
+				? `Only <strong>${timeAsStringArray[0]}</strong> left!`
 				: '';
 
 		setTimeout(updateCountdown, 1000);
@@ -40,7 +51,14 @@
 	<h1>Welcome to JSCraftCamp 2023! 🎉</h1>
 
 	<section>
-		<InfoBox title="When?">June 30th & July 1st, 2023. Registration will open on May 1st, 2023. {$countdown}</InfoBox>
+		<InfoBox title="When?">
+			<p>June 30th & July 1st, 2023.</p>
+			{#if !$canRegister}
+				<p>Registration opens on May 1st, 2023. {@html $countdown}</p>
+			{:else}
+				<p><a href="{base}/registration">Registration is open!</a></p>
+			{/if}
+		</InfoBox>
 		<InfoBox title="Where?">
 			codecentric offices Munich, <a
 				href="https://www.ecosia.org/search?method=index&q=August-Everding-Str+20+81671+M%C3%BCnchen"
@@ -54,6 +72,10 @@
 			>
 			<a href="https://github.com/jscraftcamp/website" rel="external"
 				><img src={github} alt="GitHub" /></a
+			>
+			<a
+				href="https://www.linkedin.com/events/jscraftcamp2023-jscc237033512998556028929/comments/"
+				rel="external"><img src={linkedIn} alt="LinkedIn" height="128" width="128" /></a
 			>
 			<a href="https://discord.gg/3mHhaXVEjP" rel="external"><img src={discord} alt="Discord" /></a>
 		</div>
