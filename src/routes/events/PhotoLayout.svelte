7bdcbf583d4356994bc105da5d14a78ad7cb0567
Joern Bernhardt <joern.bernhardt@compose.us>
2023-04-10 04:37:05 +0200

    feat: Style photo gallery pages
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/events/PhotoLayout.svelte
+++ src/routes/events/PhotoLayout.svelte
@@ -5,33 +5,66 @@
 
 	export let photos: string[];
 	export let year: string;
+
+	let activePhoto: string | null = null;
+
+	function setOrUnsetPhoto(photo: string) {
+		if (activePhoto === photo) {
+			activePhoto = null;
+		} else {
+			activePhoto = photo;
+		}
+	}
 </script>
 
 <PageLayout>
 	<InfoBox title="Impressions of JSCraftCamp {year}">
-		<div>
+		<div class="images">
 			{#each photos as photo, index}
-				<img
-					src="{photo}"
-					alt="Impression {index + 1} of {photos.length} from JSCraftCamp {year}"
-				/>
+				<div class="image" class:active={activePhoto === photo}>
+					<img
+						src={photo}
+						alt="Impression {index + 1} of {photos.length} from JSCraftCamp {year}"
+						on:click={() => setOrUnsetPhoto(photo)}
+						on:keypress={() => setOrUnsetPhoto(photo)}
+					/>
+				</div>
 			{/each}
 		</div>
 	</InfoBox>
 </PageLayout>
 
 <style>
-	div {
+	.images {
 		display: flex;
 		flex-flow: row wrap;
 		align-items: center;
 		justify-content: space-between;
 		gap: 2em;
 	}
-	img {
+	.image {
 		height: auto;
 		max-height: 10em;
 		max-width: 10em;
 		width: auto;
+	}
+	img {
+		display: block;
+		height: auto;
+		max-height: 100%;
+		max-width: 100%;
+	}
+	.active.image {
+		position: fixed;
+		background: #000c;
+		align-items: center;
+		justify-content: center;
+		display: flex;
+		padding: 2em;
+		inset: 0;
+		max-height: unset;
+		max-width: unset;
+		height: 100%;
+		width: 100%;
 	}
 </style>
