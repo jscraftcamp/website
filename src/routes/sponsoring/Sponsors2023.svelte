bf21a96199b4fc5d94b9b7ae7813265f5bced9fc
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-06-22 15:07:42 +0200

    feat: Replace hetzner logo
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/sponsoring/Sponsors2023.svelte
+++ src/routes/sponsoring/Sponsors2023.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import codecentric from './sponsors-2023/codecentric.png';
 	import composeus from './sponsors-2023/compose-us.svg';
-	import hetzner from './sponsors-2023/hetzner-logo.svg';
+	import hetzner from './sponsors-2023/hetzner-logo-slogan_red-square.png';
 	import inovex from './sponsors-2023/inovex-logo.png';
 	import jambit from './sponsors-2023/jambit.svg';
 	import peerigon from './sponsors-2023/peerigon.svg';
