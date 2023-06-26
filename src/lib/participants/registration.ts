7d74336913ae0d1604ac0ff344bb0865453e1b68
Wolfram Kriesing <wolfram+github@kriesing.de>
2023-06-26 19:13:03 +0200

    Registration closes 👋🏽 (#1686)
    see you all there :)

--- src/lib/participants/registration.ts
+++ src/lib/participants/registration.ts
@@ -1,6 +1,6 @@
 export const registrationOpensAt = +new Date('2023-05-01 00:00:00Z');
 export const eventStartsAt = +new Date('2023-06-30 09:30:00Z');
-export const registrationClosessAt = +new Date('2023-07-01 18:00:00Z');
+export const registrationClosessAt = +new Date('2023-06-26 17:00:00Z');
 export const isRegistrationOpen = () => {
 	const now = +new Date();
 	return registrationOpensAt <= now && now <= registrationClosessAt;
