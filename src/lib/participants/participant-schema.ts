810da23183e3018ab3fd3d70a63198df7ff0c8aa
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-04-20 19:14:13 +0200

    chore: Add LinkedIn to participants, replace Twitter with X
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/lib/participants/participant-schema.ts
+++ src/lib/participants/participant-schema.ts
@@ -43,7 +43,7 @@ export const ParticipantSchema = z
 				saturday: z.boolean()
 			})
 			.refine(
-				({ friday, saturday }) => friday || saturday,
+				({ friday, saturday }: { friday: boolean; saturday: boolean }) => friday || saturday,
 				'Either Friday or Saturday must be set to true'
 			),
 		iCanTakeNotesDuringSessions: z.boolean(),
@@ -57,13 +57,13 @@ export const ParticipantSchema = z
 			(v) => String(v).toUpperCase(),
 			z.enum(['S', 'M', 'L', 'XL', 'XXL']).nullish()
 		),
-		twitter: z.preprocess(
+		X: z.preprocess(
 			emptyToNull,
 			z
 				.string()
 				.regex(
 					/^[a-zA-Z_]{1}[a-zA-Z0-9_]{0,14}$/,
-					'Must be a valid twitter handle according to regex: /^[a-zA-Z_]{1}[a-zA-Z0-9_]{0,14}$/ (see https://regex101.com/r/B3WOro/1)'
+					'Must be a valid X handle according to regex: /^[a-zA-Z_]{1}[a-zA-Z0-9_]{0,14}$/ (see https://regex101.com/r/B3WOro/1)'
 				)
 				.nullish()
 		),
--- src/routes/participants/+page.server.ts
+++ src/routes/participants/+page.server.ts
@@ -1,19 +1,25 @@
 import { type Participant, PARTICIPANTS_DIRECTORY } from '$lib/participants/participant-schema';
 import type { PageServerLoad } from './$types';
 import { loadParticipantJsonFilePaths, loadParticipants } from '$lib/participants/participants';
+import { displayName } from '$lib/participants/display-name';
 
 export const load: PageServerLoad = async (): Promise<{ participants: Participant[] }> => {
 	try {
 		const participantsFilePaths = await loadParticipantJsonFilePaths(PARTICIPANTS_DIRECTORY);
 
 		const participants = await loadParticipants(participantsFilePaths);
-		participants.sort((a, b) =>
-			a.realName.givenName < b.realName.givenName
-				? -1
-				: a.realName.givenName === b.realName.givenName
-				? 0
-				: 1
-		);
+
+		participants.sort((a, b) => {
+			const aName = displayName(a);
+			const bName = displayName(b);
+			if (aName < bName) {
+				return -1;
+			}
+			if (bName < aName) {
+				return 1;
+			}
+			return 0;
+		});
 
 		return { participants };
 	} catch (err) {
