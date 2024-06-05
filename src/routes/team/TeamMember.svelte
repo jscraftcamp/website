fa9a75fe714b35d7d390f9aed36ccf4dc2e797f5
Philip Saa <cowglow@gmail.com>
2024-06-05 22:58:38 +0200

    Prettier

--- src/routes/team/TeamMember.svelte
+++ src/routes/team/TeamMember.svelte
@@ -1,43 +1,43 @@
-<script lang="ts">
-    import Box from '$lib/layout/Box.svelte';
-    import linkedinLogo from '$lib/participants/linkedin.svg';
+<script lang='ts'>
+	import Box from '$lib/layout/Box.svelte';
+	import linkedinLogo from '$lib/participants/linkedin.svg';
 
-    export let email: string = '';
-    export let linkedin: string | undefined = undefined;
-    export let image: string | undefined = undefined;
-    export let name: string;
+	export let email: string = '';
+	export let linkedin: string | undefined = undefined;
+	export let image: string | undefined = undefined;
+	export let name: string;
 </script>
 
 <Box>
-    <div class="member">
-        <div class="image">
-            {#if image}
-                <img src={image} alt={name}/>
-            {:else}
-                <span>🧑‍💻</span>
-            {/if}
-        </div>
-        <div class="infos">
-            <strong class="name-with-logo">
-                {name}
-                {#if linkedin}
-                    <a
-                            href={linkedin}
-                            rel="external"
-                            class="linkedin-link"
-                            title="Link to {name} on LinkedIn"
-                    >
-                        <img src={linkedinLogo} height="32" width="32" alt="Link to {name} on LinkedIn"/>
-                    </a>
-                {/if}
-            </strong>
-            {#if email}
-                <a href="mailto:{email}" title={email}>{email}</a>
-            {:else}
-                <span>&nbsp;</span>
-            {/if}
-        </div>
-    </div>
+	<div class='member'>
+		<div class='image'>
+			{#if image}
+				<img src={image} alt={name} />
+			{:else}
+				<span>🧑‍💻</span>
+			{/if}
+		</div>
+		<div class='infos'>
+			<strong class='name-with-logo'>
+				{name}
+				{#if linkedin}
+					<a
+						href={linkedin}
+						rel='external'
+						class='linkedin-link'
+						title='Link to {name} on LinkedIn'
+					>
+						<img src={linkedinLogo} height='32' width='32' alt='Link to {name} on LinkedIn' />
+					</a>
+				{/if}
+			</strong>
+			{#if email}
+				<a href='mailto:{email}' title={email}>{email}</a>
+			{:else}
+				<span>&nbsp;</span>
+			{/if}
+		</div>
+	</div>
 </Box>
 
 <style>
@@ -74,9 +74,11 @@
         height: 15em;
         overflow: hidden;
     }
-    .image:has(span){
+
+    .image:has(span) {
         place-content: center;
     }
+
     .image > img {
         width: 100%;
         height: 100%;
