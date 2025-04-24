914c1811832492b1add7ae30c11a4cf5aba82030
wolframkriesing <wolfram+github@kriesing.de>
2025-04-24 10:52:24 +0300

    The build script that makes the site run and the original design

---
+++ TODO.md
@@ -0,0 +1,21 @@
+# The min JSCC25 site
+
+- [x] make nav work, hover, active page highlight and link socials
+- [x] update hero
+  - [x] with registration date and link
+  - [x] with number of days countdown
+- [x] show companies that are sponsoring
+- [x] show map with location
+- [x] show link to sponsoring page
+- [x] show venue on the home page
+- [x] show all sponsors on the home page
+- [x] create sponsoring page
+- [x] create values page
+- [x] registration page, where its described how registration works
+- [x] imprint, etc links, footer
+- [ ] make it mobile ready
+
+## Nice to have
+- [ ] photos component (see michis-design.png)
+- [ ] partners and friends section
+- [ ] the fancy registration box, below the photos 
---
+++ build.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+import {copyFile, mkdir, readdir, readFile, stat, writeFile} from 'node:fs/promises';
+import {join} from 'node:path';
+
+const buildPage = async (componentsDir, templateFile, outputDir, outputFilename = 'index.html') => {
+  await mkdir(outputDir, {recursive: true});
+  try {
+    let output = await readFile(templateFile, 'utf-8');
+    const components = await readdir(componentsDir);
+    for (const component of components) {
+      const componentPath = join(componentsDir, component);
+      if ((await stat(componentPath)).isDirectory()) {
+        const componentName = component.split('-').pop();
+        const content = await readFile(join(componentPath, 'index.html'), 'utf-8');
+        output = output.replace(`{{ ${componentName} }}`, content);
+      }
+    }
+    const outputFile = join(outputDir, outputFilename);
+    const writingPromise = writeFile(outputFile, output);
+    console.log(`Writing ${outputFile}`);
+    return writingPromise;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+const buildPages = async (componentsDir, srcDir, outputDir) => {
+  const pages = await readdir(srcDir);
+  const writingPromises = [];
+  writingPromises.push(buildPage(componentsDir, join(srcDir, 'index.html'), outputDir));
+  for (const page of pages) {
+    const pagePath = join(srcDir, page);
+    if ((await stat(pagePath)).isDirectory()) {
+      const outputPageDir = join(outputDir, page);
+      writingPromises.push(buildPage(componentsDir, join(pagePath, 'index.html'), outputPageDir));
+    }
+  }
+  return Promise.all(writingPromises);
+}
+
+const buildCssFiles = async (componentsDir, outputDir) => {
+  const components = await readdir(componentsDir);
+  const allContent = [];
+  for (const component of components) {
+    const componentPath = join(componentsDir, component);
+    allContent.push(await readFile(join(componentPath, 'style.css'), 'utf-8'));
+  }
+  const outputFile = join(outputDir, 'components.css');
+  const writingPromise = writeFile(outputFile, allContent.join('\n'));
+  console.log(`Writing ${outputFile}`);
+  return writingPromise;
+};
+
+const SRC_DIR = 'src/pages';
+const COMPONENTS_DIR = 'src/components';
+const DIST_DIR = '_dist';
+
+const start = Date.now();
+Promise.all([
+  buildPages(COMPONENTS_DIR, SRC_DIR, DIST_DIR),
+  buildCssFiles(COMPONENTS_DIR, DIST_DIR),
+  copyFile(join(SRC_DIR, 'reset.css'), join(DIST_DIR, 'reset.css')),
+  copyFile(join(SRC_DIR, 'global.js'), join(DIST_DIR, 'global.js')),
+]).then(() => {
+  const duration = Date.now() - start;
+  console.log(`[${new Date().toISOString()}] Build completed. Took ${duration}ms`);
+});
