6e43335753ac8f28d9a003f632269dcad1a1c939
wolframkriesing <wolfram+github@kriesing.de>
2025-04-24 10:51:52 +0300

    Add all the components and pages

---
+++ src/pages/global.js
@@ -0,0 +1,5 @@
+const $ = (...args) => document.querySelector(...args);
+const $$ = (...args) => document.querySelectorAll(...args);
+
+const JSCC_START_DATE = '2025-06-27 09:00';
+const REGISTRATION_START_DATE = '2025-04-30 00:00';
