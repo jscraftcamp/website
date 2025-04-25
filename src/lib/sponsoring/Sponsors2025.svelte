ce2492a9b33022127dadade476f2fa2596170949
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-04-25 12:24:10 +0200

    chore: Add sponsors
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/lib/sponsoring/Sponsors2025.svelte
+++ src/lib/sponsoring/Sponsors2025.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import type { Sponsor } from './is-sponsor';
 	import codecentric from './sponsors-2025/codecentric.png';
+	import composeus from './sponsors-2025/compose-us.svg';
+	import deno from './sponsors-2025/deno-logo.svg';
+	import peerigon from './sponsors-2025/peerigon.svg';
 	import saab from './sponsors-2025/saab.svg';
+	import typedigital from './sponsors-2025/typedigital.svg';
 	import Sponsors from './Sponsors.svelte';
 
 	const sponsors: Array<Sponsor> = [
@@ -12,10 +16,34 @@
 			sponsoring: 'Location'
 		},
 		{
+			image: composeus,
+			link: 'https://compose.us',
+			name: 'compose.us',
+			sponsoring: 'Hosted by'
+		},
+		{
+			image: deno,
+			link: 'https://deno.land',
+			name: 'deno',
+			sponsoring: '250 €'
+		},
+		{
+			image: peerigon,
+			link: 'https://peerigon.com',
+			name: 'peerigon',
+			sponsoring: 'Coffee / Barista'
+		},
+		{
 			image: saab,
 			link: 'https://www.saab.com',
 			name: 'Saab',
 			sponsoring: 'Breakfast'
+		},
+		{
+			image: typedigital,
+			link: 'https://typedigital.de',
+			name: 'typedigital',
+			sponsoring: 'Friday Snacks'
 		}
 	].sort((a, b) => a.name.localeCompare(b.name));
 </script>
