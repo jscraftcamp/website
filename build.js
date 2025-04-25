7b4ea07b0892afffc06e456d03feaf2ad0bbb6bd
wolframkriesing <wolfram+github@kriesing.de>
2025-04-25 13:28:00 +0300

    Add the sponsor logos

--- build.js
+++ build.js
@@ -52,17 +52,25 @@ const buildCssFiles = async (componentsDir, outputDir) => {
   return writingPromise;
 };
 
-const SRC_DIR = 'src/pages';
+const PAGES_DIR = 'src/pages';
+const SPONSOR_LOGOS_DIR = 'src/lib/sponsoring/sponsors-2025';
 const COMPONENTS_DIR = 'src/components';
 const DIST_DIR = '_dist';
 
 const start = Date.now();
 await mkdir(DIST_DIR, {recursive: true});
+await mkdir(join(DIST_DIR, 'img/sponsors'), {recursive: true});
 Promise.all([
-  buildPages(COMPONENTS_DIR, SRC_DIR, DIST_DIR),
+  buildPages(COMPONENTS_DIR, PAGES_DIR, DIST_DIR),
   buildCssFiles(COMPONENTS_DIR, DIST_DIR),
-  copyFile(join(SRC_DIR, 'reset.css'), join(DIST_DIR, 'reset.css')),
-  copyFile(join(SRC_DIR, 'global.js'), join(DIST_DIR, 'global.js')),
+  copyFile(join(PAGES_DIR, 'reset.css'), join(DIST_DIR, 'reset.css')),
+  copyFile(join(PAGES_DIR, 'global.js'), join(DIST_DIR, 'global.js')),
+  
+  // the sponsors
+  copyFile(join(SPONSOR_LOGOS_DIR, 'codecentric.webp'), join(DIST_DIR, 'img/sponsors/codecentric.webp')),
+  copyFile(join(SPONSOR_LOGOS_DIR, 'deno-logo.svg'), join(DIST_DIR, 'img/sponsors/deno-logo.svg')),
+  copyFile(join(SPONSOR_LOGOS_DIR, 'saab.svg'), join(DIST_DIR, 'img/sponsors/saab.svg')),
+  
 ]).then(() => {
   const duration = Date.now() - start;
   console.log(`[${new Date().toISOString()}] Build completed. Took ${duration}ms`);
