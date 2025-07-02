72e62c316c2044b7393ec04dab72b99b16c41349
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-07-02 10:15:39 +0200

    feat: Add /participants/stats.json route to get current stats
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/participants/stats.json/+server.ts
@@ -0,0 +1,14 @@
+import { loadAllParticipants, orgaMembers } from '$lib/participants/participants';
+import { createStatsFromParticipants } from '$lib/participants/statistics';
+import { json, type RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async () => {
+	try {
+		const participants = await loadAllParticipants();
+		const stats = createStatsFromParticipants(participants, orgaMembers);
+		return json(stats);
+	} catch (err) {
+		console.error('Loading of participants failed:', err);
+		throw err;
+	}
+};
