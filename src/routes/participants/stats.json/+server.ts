a11f073e3ee849bb4ed9b9bec79dbc0cb58183d0
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-07-02 22:52:07 +0200

    fix: Ensure stats.json are prerenderable
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/participants/stats.json/+server.ts
+++ src/routes/participants/stats.json/+server.ts
@@ -2,6 +2,8 @@ import { loadAllParticipants, orgaMembers } from '$lib/participants/participants
 import { createStatsFromParticipants } from '$lib/participants/statistics';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
+export const prerender = true;
+
 export const GET: RequestHandler = async () => {
 	try {
 		const participants = await loadAllParticipants();
