2cb78e45773951350f19e35a25624151d07d9f84
Tobias Sittenauer <tobias@sittenauer.eu>
2026-02-01 00:45:22 +0100

    feat: unconference card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import SponsorRequired from '$lib/components/SponsorRequired.svelte';
 	import SponsorCard from '$lib/components/SponsorCard.svelte';
 	import Partners from '$lib/components/Partners.svelte';
+	import UnconferenceDescription from '$lib/components/UnconferenceDescription.svelte';
 </script>
 
 <PageLayout>
@@ -11,6 +12,7 @@
 		<Header />
 		<SponsorRequired class="lg:w-100" />
 		<SponsorCard class="lg:col-span-2" />
+		<UnconferenceDescription class="lg:w-100" />
 	</div>
 	<Partners />
 </PageLayout>
