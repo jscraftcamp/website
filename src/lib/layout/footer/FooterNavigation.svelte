4c7e286e1f231b2111b245644c6a04ba8aa53cc6
Tobias Sittenauer <tobias@sittenauer.eu>
2026-01-31 19:52:38 +0100

    refactor: use text-white/ pattern in footer

--- src/lib/layout/footer/FooterNavigation.svelte
+++ src/lib/layout/footer/FooterNavigation.svelte
@@ -1,28 +1,29 @@
 <script lang="ts">
 	import { base } from '$app/paths';
-	import Logo from '../Logo';
+	import Logo from '../../logos/CurrentLogo';
 	import background from './background.svg';
 </script>
 
 <footer
 	class="relative flex min-h-80 flex-col items-center overflow-hidden px-8 pt-8 pb-16 md:min-h-120"
 >
-	<nav class="z-10 flex items-center gap-3 font-medium text-white">
+	<nav class="z-10 flex items-center gap-3 font-medium text-white/50">
 		<a
 			href="{base}/legal"
-			class="no-underline opacity-50 transition-opacity duration-200 hover:underline hover:opacity-80"
+			class="no-underline opacity-50 transition-opacity duration-200 hover:text-white/80 hover:underline"
 			>Imprint</a
 		>
 		<span class="opacity-50">·</span>
 		<a
 			href="{base}/legal#datenschutz"
-			class="opacity-50 transition-opacity duration-200 hover:underline hover:opacity-80"
+			class="opacity-50 transition-opacity duration-200 hover:text-white/80 hover:underline"
 			>Privacy Policy</a
 		>
 		<span class="opacity-50">·</span>
 		<a
 			href="{base}/archive"
-			class="opacity-50 transition-opacity duration-200 hover:underline hover:opacity-80">Archive</a
+			class="opacity-50 transition-opacity duration-200 hover:text-white/80 hover:underline"
+			>Archive</a
 		>
 	</nav>
 	<div class="z-10 mt-12">
