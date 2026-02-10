df37755d75fd6f3d6e725d91029714aeb12d5371
wolframkriesing <wolfram+github@kriesing.de>
2026-02-11 00:16:48 +0100

    Put the venue in the config. Missing: on the home page

--- src/lib/config/venue/venue.ts
+++ src/lib/config/venue/venue.ts
@@ -1,18 +1,18 @@
 import type { VenueConfig } from './types';
 
 export const venueConfig: VenueConfig = {
-	noVenueAvailable: true,
+	noVenueAvailable: false,
 
 	address: {
-		name: 'codecentric AG',
-		street: 'August-Everding-Straße 20',
-		city: '81671 München',
-		mapsLink: 'https://maps.app.goo.gl/P2V1zn797ffFdjPm9'
+		name: 'TNG Technology Consulting GmbH',
+		street: 'Beta-Straße 13',
+		city: '85774 Unterföhring',
+		mapsLink: 'https://www.openstreetmap.org/search?query=Beta-Stra%C3%9Fe+13+85774+Unterf%C3%B6hring&zoom=6#map=19/48.185987/11.654745'
 	},
 
-	impressionsLink: 'https://www.codecentric.de/standorte/muenchen',
+	impressionsLink: 'https://www.tngtech.com/ueber-uns/standorte/unterfoehring-deutschland/',
 
-	description: "This year's location will be at the codecentric München office.",
+	description: "This year's location will be at the TNG office in Munich (Unterföhring).",
 
 	accessibility: {
 		description: "We'll update this when we have more information available."
