ac0cfb0ca1d4074ee5b19be08f9fd7d1d2e50cbe
wolframkriesing <wolfram+github@kriesing.de>
2026-02-11 00:10:45 +0100

    Currently TNG is the only known sponsor.

--- src/lib/config/sponsoring/sponsoring.ts
+++ src/lib/config/sponsoring/sponsoring.ts
@@ -24,102 +24,102 @@ export const sponsors: Sponsor[] = [
 		name: 'StackForge',
 		sponsoring: 'Breakfast Fr & Sa'
 	},*/
-	{
-		image: allPrintCut,
-		link: 'https://allprintcut.com/',
-		name: 'All Print Cut',
-		sponsoring: 'Stickers'
-	},
-	{
-		image: alm,
-		link: 'https://alm.sh',
-		name: 'Alm Engineering',
-		sponsoring: '500 €'
-	},
-	{
-		image: codecentric,
-		link: 'https://www.codecentric.de',
-		name: 'codecentric AG',
-		sponsoring: 'Location'
-	},
-	{
-		image: composeus,
-		link: 'https://compose.us',
-		name: 'compose.us',
-		sponsoring: 'Hosted by'
-	},
-	{
-		image: deno,
-		link: 'https://deno.land',
-		name: 'deno',
-		sponsoring: '250 €'
-	},
-	{
-		image: finanzvergleich,
-		link: 'https://finanzvergleich.com',
-		name: 'Finanzvergleich.com',
-		sponsoring: '200 €'
-	},
-	{
-		image: gorlug,
-		link: 'https://gorlug.de/',
-		name: 'Gorlug',
-		sponsoring: '100 €'
-	},
-	{
-		image: hetzner,
-		link: 'https://www.hetzner.com',
-		name: 'Hetzner',
-		sponsoring: '500 €'
-	},
-	{
-		image: inovex,
-		link: 'https://inovex.de',
-		name: 'inovex',
-		sponsoring: 'Lunch'
-	},
-	{
-		image: inspiredconsulting,
-		link: 'https://inspired.consulting',
-		name: 'Inspired Consulting',
-		sponsoring: '300 €'
-	},
-	{
-		image: peerigon,
-		link: 'https://peerigon.com',
-		name: 'peerigon',
-		sponsoring: 'Coffee / Barista'
-	},
-	{
-		image: saab,
-		link: 'https://www.saab.com',
-		name: 'Saab',
-		sponsoring: 'Breakfast & Energy Drinks'
-	},
-	{
-		image: tiffingerthiel,
-		link: 'https://tiffinger-thiel.de/',
-		name: 'Tiffinger & Thiel GmbH',
-		sponsoring: 'Beverages'
-	},
+	// {
+	// 	image: allPrintCut,
+	// 	link: 'https://allprintcut.com/',
+	// 	name: 'All Print Cut',
+	// 	sponsoring: 'Stickers'
+	// },
+	// {
+	// 	image: alm,
+	// 	link: 'https://alm.sh',
+	// 	name: 'Alm Engineering',
+	// 	sponsoring: '500 €'
+	// },
+	// {
+	// 	image: codecentric,
+	// 	link: 'https://www.codecentric.de',
+	// 	name: 'codecentric AG',
+	// 	sponsoring: 'Location'
+	// },
+	// {
+	// 	image: composeus,
+	// 	link: 'https://compose.us',
+	// 	name: 'compose.us',
+	// 	sponsoring: 'Hosted by'
+	// },
+	// {
+	// 	image: deno,
+	// 	link: 'https://deno.land',
+	// 	name: 'deno',
+	// 	sponsoring: '250 €'
+	// },
+	// {
+	// 	image: finanzvergleich,
+	// 	link: 'https://finanzvergleich.com',
+	// 	name: 'Finanzvergleich.com',
+	// 	sponsoring: '200 €'
+	// },
+	// {
+	// 	image: gorlug,
+	// 	link: 'https://gorlug.de/',
+	// 	name: 'Gorlug',
+	// 	sponsoring: '100 €'
+	// },
+	// {
+	// 	image: hetzner,
+	// 	link: 'https://www.hetzner.com',
+	// 	name: 'Hetzner',
+	// 	sponsoring: '500 €'
+	// },
+	// {
+	// 	image: inovex,
+	// 	link: 'https://inovex.de',
+	// 	name: 'inovex',
+	// 	sponsoring: 'Lunch'
+	// },
+	// {
+	// 	image: inspiredconsulting,
+	// 	link: 'https://inspired.consulting',
+	// 	name: 'Inspired Consulting',
+	// 	sponsoring: '300 €'
+	// },
+	// {
+	// 	image: peerigon,
+	// 	link: 'https://peerigon.com',
+	// 	name: 'peerigon',
+	// 	sponsoring: 'Coffee / Barista'
+	// },
+	// {
+	// 	image: saab,
+	// 	link: 'https://www.saab.com',
+	// 	name: 'Saab',
+	// 	sponsoring: 'Breakfast & Energy Drinks'
+	// },
+	// {
+	// 	image: tiffingerthiel,
+	// 	link: 'https://tiffinger-thiel.de/',
+	// 	name: 'Tiffinger & Thiel GmbH',
+	// 	sponsoring: 'Beverages'
+	// },
 	{
 		image: tng,
 		link: 'https://www.tngtech.com/',
 		name: 'TNG Technology Consulting',
 		sponsoring: '500 €'
 	},
-	{
-		image: twilio,
-		link: 'https://twilio.com/',
-		name: 'Twilio',
-		sponsoring: '1.500 $'
-	},
-	{
-		image: typedigital,
-		link: 'https://typedigital.de',
-		name: 'typedigital',
-		sponsoring: 'Friday Snacks'
-	}
+	// {
+	// 	image: twilio,
+	// 	link: 'https://twilio.com/',
+	// 	name: 'Twilio',
+	// 	sponsoring: '1.500 $'
+	// },
+	// {
+	// 	image: typedigital,
+	// 	link: 'https://typedigital.de',
+	// 	name: 'typedigital',
+	// 	sponsoring: 'Friday Snacks'
+	// }
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export const isSponsor = (key: string) => {
