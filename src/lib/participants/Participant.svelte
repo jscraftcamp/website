dc4ea95fec7b0d7d3ee4c2ce3f98aba144d3bf84
Tobias Sittenauer <tobias.sittenauer@stack-forge.eu>
2026-04-14 00:46:24 +0200

    feat: add a flat crown icon

--- src/lib/participants/Participant.svelte
+++ src/lib/participants/Participant.svelte
@@ -6,6 +6,7 @@
 	import xLogo from '$lib/icons/x.svg?raw';
 	import globeIcon from '$lib/icons/globe.svg?raw';
 	import pencilSquareIcon from '$lib/icons/pencil-square.svg?raw';
+	import crownIcon from '$lib/icons/crown.svg?raw';
 	import type { Participant } from '$lib/participants/participant-schema';
 	import { displayName } from '$lib/participants/display-name';
 	import { isSponsor } from '$lib/sponsoring/is-sponsor';
@@ -47,7 +48,11 @@
 			aria-controls="participant-details-{participant.githubAccountName}"
 			class="font-inherit cursor-pointer border-none bg-transparent p-0 text-left text-primary-700 uppercase focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 focus:ring-offset-black focus:outline-none"
 		>
-			{#if participantIsSponsor}<span title="Sponsor">👑</span>{/if}{displayName(participant)}
+			{#if participantIsSponsor}<span
+					class="-mt-1 mr-1 inline-block h-4 w-4 align-middle text-primary-500 *:size-full"
+					title="Sponsor"
+					aria-label="Sponsor">{@html crownIcon}</span
+				>{/if}{displayName(participant)}
 		</button>
 	</h3>
 
